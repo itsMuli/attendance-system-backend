a35e3d4b8f52064d2703355e84fd8a4be00f9ec5
Rose Muli <rosemwendwa163@gmail.com>
2022-09-30 16:34:46 +0300

    jwt

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+app.use(credentials);
 
 app.use(cors(corsOptions));
 
